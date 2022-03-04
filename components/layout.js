32f598108120fc4b7d3df0019f8695ed44f0918b
Albert Chang <a89529294@hotmail.com>
2022-03-04 10:57:29 +0800

    change site index page title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Albert Chang";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Next.js Blog";
 
 export default function Layout({ children, home }) {
   return (
